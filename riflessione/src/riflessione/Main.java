34f688c9ef28a915a07d552cd87e8b1eabe1bd04
cicciorose <francesco.rose@hotmail.it>
2016-07-20 18:34:46 +0200

    complete second method

--- riflessione/src/riflessione/Main.java
+++ riflessione/src/riflessione/Main.java
@@ -10,16 +10,30 @@ import java.util.List;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
 
+import sun.tools.jar.resources.jar;
+
 
 public class Main {
-	
+	/**
+	 * Returns list of all the classes in the jar
+	 * 
+	 * @param pathToJar Path of the jar 
+	 * 
+	 * @return List&ltClass&lt?&gt&gt list of all the classes in the jar   
+	 * 
+	 * @author Francesco Rose
+	 * 
+	 * @exception ClassNotFoundException
+	 * 
+	 * @exception IOException
+	 * 
+	 * @since 1.0
+	 * 
+	 * @see java.lang.reflect
+	 * 
+	 *  
+	 * */
 	public static List<Class<?>> loadClass(String pathToJar)throws IOException, ClassNotFoundException{
-/*		File dir = new File("file");//		
-//		for (File file : dir.listFiles()){
-//			System.out.println(file.getPath());
-//			jarLoader.addFile(file.getPath());			
-		}String pathToJar="file/test.jar";
-*/
 		
 		List<Class<?>> list=new ArrayList<Class<?>>();
 		
@@ -41,23 +55,52 @@ public class Main {
 		jarFile.close();
 		return list;
 	}
-
+	/**
+	 * Rreturns the class named <code>name</code> if this is possible
+	 * 
+	 * @param pathToJar Path of the jar 
+	 * 
+	 * @param name Name of class
+	 * 
+	 * @return Class&lt?&gt class named name in the jar 
+	 * 
+	 * @author Francesco Rose
+	 * 
+	 * @exception ClassNotFoundException
+	 * 
+	 * @exception IOException
+	 * 
+	 * @since 1.0
+	 * 
+	 * @see java.lang.reflect
+	 *   
+	 * */
+	public static Class<?> loadClassForName(String pathToJar,String name) throws IOException, ClassNotFoundException{
+		
+		Class<?> classe=null;
+		JarFile jarFile = new JarFile(pathToJar);
+		URL[] urls = { new URL("jar:file:" + pathToJar+"!/") };
+		URLClassLoader cl = URLClassLoader.newInstance(urls);
+		classe=cl.loadClass(name);		
+		jarFile.close();
+		return classe;
+	}
+	/**
+	 * EXAMPLE load all class and class for name
+	 * 
+	 * @return void ,print two method
+	 * 
+	 * 
+	 * */
 	public static void main(String[] args) throws IOException, ClassNotFoundException {
 		
-		List<Class<?>>classes=Main.loadClass("file/test.jar");
-		
-		Class<?> nuova=classes.get(0);
-		
-		
+		List<Class<?>>classes=Main.loadClass("file/test.jar");		
+		Class<?> nuova=classes.get(0);		
 		if(nuova.getSuperclass()==ProvaAstratta.class){
 			System.out.println("yep");
-		}
-		
-		try {
-			
-			
-			ProvaAstratta p=(ProvaAstratta)nuova.newInstance();
-			
+		}		
+		try {			
+			ProvaAstratta p=(ProvaAstratta)nuova.newInstance();			
 			System.out.println(p.getNome()+" "+p.getTipo());
 			
 		} catch (InstantiationException | IllegalAccessException e) {
@@ -66,6 +109,15 @@ public class Main {
 		//STAMPA
 		//yep
 		//ciccio concreto
+		Class<?> daNome=Main.loadClassForName("file/test.jar", "riflessione.ProvaConcreta");
+		try {
+			ProvaAstratta p=(ProvaAstratta) daNome.newInstance();
+			
+			System.err.println(p.getNome()+" "+p.getTipo());
+			
+		} catch (InstantiationException | IllegalAccessException e) {
+			e.printStackTrace();
+		}	
 
 	}
 
